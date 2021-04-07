700360b358b685aa18341f94d13205de0f153faa
Hyunsoo Kim <khs8319@chol.com>
2021-04-07 11:48:00 +0900

    나의 첫번째 커밋

---
+++ git_xcode/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  git_xcode
+//
+//  Created by khs8319 on 2021/04/07.
+//  Copyright © 2021 khs8319. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    // insert code here...
+    
+    
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, Hyunsoo!\n";
+    
+    return 0;
+}
